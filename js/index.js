d0f09866c158457a9c1195b78a64e3098903d602
marco <zmarco.20.ac@gmail.com>
2026-03-30 16:04:17 -0500

    Animacion y Nuevo banner

--- js/index.js
+++ js/index.js
@@ -5,6 +5,17 @@ const header = document.getElementById("font-header");
 const menu = document.getElementById("group-2");
 const burguer = document.getElementById("burguer");
 const close = document.getElementById("close");
+
+const about = document.getElementById("about");
+const allAbout = document.getElementById("all-about");
+
+const process = document.getElementById("process");
+const allProcess = document.getElementById("all-process");
+const processImg = document.getElementById("process-img");
+
+const services = document.getElementById("services");
+const allServices = document.getElementById("all-services");
+
 const im = document.getElementById("im");
 const cm = document.getElementById("cm");
 
@@ -28,35 +39,32 @@ const msgReparaciones =
 const msgGeneral =
   "Hola Antonio, vi su página web. Necesito información sobre sus servicios. 🙂";
 
-const observer = new IntersectionObserver(
-  (entries) => {
-    if (!entries[0].isIntersecting) {
-      header.classList.add("active");
-    } else {
-      header.classList.remove("active");
-    }
-  },
-  {
-    root: null,
-    threshold: 0.8,
-  },
-);
+function createObserver(element, alcance = 0) {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      if (entries[0].isIntersecting) {
+        element.classList.add("active");
+      } else {
+        element.classList.remove("active");
+      }
+    },
+    {
+      root: null,
+      threshold: alcance,
+    },
+  );
+  return observer;
+}
 
-observer.observe(banner);
-
-burguer.addEventListener("click", () => {
-  console.log("Burguer");
-  menu.classList.add("active");
-});
+const observerHeader = createObserver(header, 0.8);
+const observerAbout = createObserver(allAbout, 0.2);
+const observerProcess = createObserver(allProcess, 0.2);
+const observerServices = createObserver(allServices, 0.2);
 
 function Close() {
   console.log("Cancel");
   menu.classList.remove("active");
 }
-
-close.addEventListener("click", Close);
-im.addEventListener("click", Close);
-cm.addEventListener("click", Close);
 
 function openWhatsApp(message) {
   const encodedMsg = encodeURIComponent(message);
@@ -71,9 +79,39 @@ function openWhatsApp(message) {
   }
 }
 
+function checkWidth() {
+  if (window.innerWidth < 670) {
+    console.log("phone");
+    processImg.src = "assets/img/banner-phone.png";
+  } else if (window.innerWidth <= 980) {
+    processImg.src = "assets/img/banner-tablet.png";
+    console.log("tablet");
+  } else {
+    processImg.src = "assets/img/banner-pc.png";
+    console.log("pc");
+  }
+}
+
+burguer.addEventListener("click", () => {
+  console.log("Burguer");
+  menu.classList.add("active");
+});
+
+observerHeader.observe(banner);
+observerAbout.observe(about);
+observerProcess.observe(process);
+observerServices.observe(services);
+
+close.addEventListener("click", Close);
+im.addEventListener("click", Close);
+cm.addEventListener("click", Close);
+
 cb.addEventListener("click", () => openWhatsApp(msgGeneral));
 eb.addEventListener("click", () => openWhatsApp(msgElectricidad));
 fb.addEventListener("click", () => openWhatsApp(msgFontaneria));
 ib.addEventListener("click", () => openWhatsApp(msgInstalaciones));
 rb.addEventListener("click", () => openWhatsApp(msgReparaciones));
 wb.addEventListener("click", () => openWhatsApp(msgGeneral));
+
+window.addEventListener("resize", checkWidth);
+checkWidth();
